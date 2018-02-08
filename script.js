b72e94feaff56078f2a6b8d0af032662c5d6a7ae
Letícia Mazzo <leticiamazzo@gmail.com>
2018-02-08 10:29:22 -0200

    botao ainda com link azul para apresentar demoday

---
+++ script.js
@@ -0,0 +1,9 @@
+// function clicar(){
+//     alert("teste com sucesso");
+// }
+
+// function MudarTexto(){
+//     var TextoDiferente = document.getElementById ('mudeiTexto').valuer;
+//     document.getElementById('novotexto').innerHTML = TextoDiferente;
+//     document.getElementById('novotexto').style.color = "orange";
+// }
